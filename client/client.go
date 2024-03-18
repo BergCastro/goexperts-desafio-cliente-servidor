94c88d4393e43c42b914455478e42389be100208
Lindemberg Nunes de Castro <lindembergncastro@outlook.com>
2024-03-17 22:49:06 -0300

    fix: correcoes de mensagens

--- client/client.go
+++ client/client.go
@@ -15,22 +15,22 @@ func main() {
 
 	req, err := http.NewRequestWithContext(ctx, "GET", "http://localhost:8080/cotacao", nil)
 	if err != nil {
-		log.Fatalf("Failed to create request: %v", err)
+		log.Fatalf("Ocorreu um ao criar a requisicao: %v", err)
 	}
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		log.Fatalf("Failed to fetch quote: %v", err)
+		log.Fatalf("Ocorreu um erro ao fazer a requisicao: %v", err)
 	}
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatalf("Failed to read response body: %v", err)
+		log.Fatalf("Ocorreu um erro ao tentar ler o body: %v", err)
 	}
 
 	err = os.WriteFile("cotacao.txt", body, 0644)
 	if err != nil {
-		log.Fatalf("Failed to save quote to file: %v", err)
+		log.Fatalf("Ocorreu um erro ao tentar salvar a contacao no arquivo: %v", err)
 	}
 }
